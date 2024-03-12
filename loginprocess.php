1b92d0534411b2d630745dcda20d9a1c69260e6e
Alicja Krupa <stasiu17322@gmail.com>
2024-03-12 10:34:39 +0000

    login php done (untested)

---
+++ loginprocess.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+$login = ($_POST);
+$email = $login["email"];
+require_once('config.inc.php');
+
+$mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[1]);
+if($mysqli -> connect_error) {
+    die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+}
+
+$hashedpw = $mysqli -> query("SELECT password FROM tblUsers WHERE email='$email'");
+
+if (password_verify($login["password"],$hashedpw)) {
+    $_SESSION["email"] = $email;
+    echo('login successful');
+}
+
+?>
