16792e61b44f1e1b527335dce1853c9d4780a927
Emma Peel <emma.peel-2@student.manchester.ac.uk>
2024-04-08 16:11:02 +0000

    Upload New File

---
+++ favourites.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="mystyles.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Spark</title>
+</head>
+<header>
+    <p style="font-weight: bold; font-size: 2em; display: inline;">Spark </p>
+	<p class="tagline">The shortest circuit to your dream laptop</p> 
+    <img src="profile.png" alt="Profile image" width="30" height="30" style="display: inline; float: right; margin-right:1%; margin-top:0.8%;">
+    <img src="checkout.png" alt="Checkout image" width="30" height="30" style="display: inline; float: right;margin-right:1%;margin-top:0.8%">
+</header>
+<body>
+
+</body>
+</html>
